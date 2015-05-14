280c2f7e84c97c51a6b88e5fca05ab24efb4fc07
BlueCocoa <445108920@qq.com>
2015-05-14 18:51:52 +0800

    Bilibili No ADs Safari Extension
    Bilibili No ADs Safari Extension

---
+++ bilibili-no-ads.safariextension/NoAd.js
@@ -0,0 +1,10 @@
+var re = /^http:\/\/www.bilibili.com\/video\/av/;
+if (re.exec(window.location.href) != null) {
+    console.log("Remove ADs.");
+    var elements = document.getElementsByTagName('*');
+    for (var i = 0; i < elements.length; i++) { 
+        if (elements[i].className == "ad-f" || elements[i].className == "left ad-fl" || elements[i].className == "right ad-fr"){ 
+            elements[i].parentNode.removeChild(elements[i]);
+        }
+    }
+}
