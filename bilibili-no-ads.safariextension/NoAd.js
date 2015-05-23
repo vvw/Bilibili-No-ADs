7cca1823cf8d0d28949cc17f157e72f923472990
BlueCocoa <445108920@qq.com>
2015-05-23 09:09:31 +0800

    Remove AD below the player
    Remove AD below the player

--- bilibili-no-ads.safariextension/NoAd.js
+++ bilibili-no-ads.safariextension/NoAd.js
@@ -3,7 +3,7 @@ if (re.exec(window.location.href) != null) {
     console.log("Remove ADs.");
     var elements = document.getElementsByTagName('*');
     for (var i = 0; i < elements.length; i++) { 
-        if (elements[i].className == "ad-f" || elements[i].className == "left ad-fl" || elements[i].className == "right ad-fr"){ 
+        if (elements[i].className == "ad-f" || elements[i].className == "left ad-fl" || elements[i].className == "right ad-fr" || elements[i].className == "bangumi-content"){ 
             elements[i].parentNode.removeChild(elements[i]);
         }
     }
